1e00e25e6aac32425a15ab971e1c3a0c97e9c2f7
jhddong0107 <kko01-s2210108@gclass.ice.go.kr>
2024-05-21 20:37:36 +0900

    배경음과 최고 점수

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Score : MonoBehaviour
 {
     public Text scoreText;
     private float time;
-    private int Int_time;
+    public int Int_time;
+    public int Bestscore;
+    public Text BestText;
 
+
+    void Start()
+    {
+       if(PlayerPrefs.HasKey("Best"))
+        {
+            Bestscore = PlayerPrefs.GetInt("Best");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
         Int_time = (int)time;
+
+        if (Int_time > Bestscore)
+        {
+            Bestscore = Int_time;
+            PlayerPrefs.SetInt("Best", Bestscore);
+
+        }
         scoreText.text = "Score : " + Int_time.ToString();
+        BestText.text = "Best :"  +  Bestscore.ToString();
+
+
+
     }
+
+
 }
+
